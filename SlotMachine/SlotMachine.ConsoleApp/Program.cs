68680e93e09c553139e315fa064ed6c6200a46b0
annix8 <mario.ivailov@abv.bg>
2019-03-27 21:05:15 +0200

    added new console project

---
+++ SlotMachine/SlotMachine.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using SlotMachine.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlotMachine.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameController = new GameController();
+        }
+    }
+}
