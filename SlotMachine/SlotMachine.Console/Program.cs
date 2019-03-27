e217a24afd8963501eb99e9dbe3cd5e1e86ddb4c
annix8 <mario.ivailov@abv.bg>
2019-03-27 21:04:06 +0200

    removed project

--- SlotMachine/SlotMachine.Console/Program.cs
+++ SlotMachine/SlotMachine.Console/Program.cs
@@ -6,7 +6,22 @@ namespace SlotMachine.Console
     {
         static void Main(string[] args)
         {
-            new GameController().RunGameLoop();
+            var gameController = new GameController();
+            gameController.CreateNewGame();
+        }
+
+        private static decimal RequestPlayerDeposit()
+        {
+            Console.WriteLine("Please deposit money you would like to play with:");
+            bool isMoney = decimal.TryParse(Console.ReadLine(), out decimal money);
+
+            while (!isMoney || money <= 0)
+            {
+                Console.WriteLine("Please enter a valid positive number");
+                isMoney = decimal.TryParse(Console.ReadLine(), out money);
+            }
+
+            return money;
         }
     }
 }
