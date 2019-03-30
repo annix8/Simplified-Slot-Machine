02ffe18810228339a65dbb6d0e93635d7d22b404
annix8 <mario.ivailov@abv.bg>
2019-03-30 10:00:31 +0200

    added console game engine class that runs the console game loop

---
+++ SlotMachine/SlotMachine.ConsoleApp/ConsoleGameEngine.cs
@@ -0,0 +1,73 @@
+﻿using SlotMachine.ConsoleApp.IO.Contracts;
+using SlotMachine.Core;
+using SlotMachine.Core.Models.Dto;
+using SlotMachine.Core.Models.Symbols;
+using System.Collections.Generic;
+
+namespace SlotMachine.ConsoleApp
+{
+    public class ConsoleGameEngine
+    {
+        private readonly IReader _reader;
+        private readonly IWriter _writer;
+        private readonly PlayerInputRequester _playerInputRequester;
+        private GameController _gameController;
+        private bool _runNewGame = true;
+
+        public ConsoleGameEngine(IReader reader,
+            IWriter writer,
+            PlayerInputRequester playerInputRequester)
+        {
+            _reader = reader;
+            _writer = writer;
+            _playerInputRequester = playerInputRequester;
+            _gameController = CreateGameController();
+        }
+
+        public void RunGameLoop()
+        {
+            while (_runNewGame)
+            {
+                decimal stake = _playerInputRequester.RequestStake();
+                SlotMachineSpinResultDto result = _gameController.SpinMachine(stake);
+
+                if (!result.IsSuccess)
+                {
+                    _writer.WriteLine(result.ResultMessage);
+                    continue;
+                }
+
+                DrawSymbols(result.Symbols);
+                _writer.WriteLine($"You have won: {result.WinAmount:f2}");
+                _writer.WriteLine($"Current balance is: {result.PlayerBalance:f2}");
+
+                if (!_gameController.PlayerCanPlay())
+                {
+                    _runNewGame = _playerInputRequester.AskForNewGame();
+
+                    if (_runNewGame)
+                    {
+                        _gameController = CreateGameController();
+                    }
+                }
+            }
+        }
+
+        private void DrawSymbols(List<List<Symbol>> rowsOfSymbols)
+        {
+            foreach (var rowOfSymbols in rowsOfSymbols)
+            {
+                _writer.WriteLine(string.Join(", ", rowOfSymbols));
+            }
+        }
+
+        private GameController CreateGameController()
+        {
+            var gameController = new GameController();
+            decimal deposit = _playerInputRequester.RequestPlayerDeposit();
+            gameController.CreateNewGame(deposit);
+
+            return gameController;
+        }
+    }
+}
--- SlotMachine/SlotMachine.ConsoleApp/Program.cs
+++ SlotMachine/SlotMachine.ConsoleApp/Program.cs
@@ -1,79 +1,23 @@
 ﻿using SlotMachine.ConsoleApp.IO;
-using SlotMachine.ConsoleApp.IO.Contracts;
-using SlotMachine.Core;
-using SlotMachine.Core.Models.Dto;
-using SlotMachine.Core.Models.Symbols;
-using System.Collections.Generic;
 
 namespace SlotMachine.ConsoleApp
 {
-    class Program
+    public class Program
     {
-        private static IReader _reader;
-        private static IWriter _writer;
-        private static GameController _gameController;
-        private static PlayerInputRequester _playerInputRequester;
-
-        private static bool _runNewGame = true;
-
         static void Main(string[] args)
         {
-            Initialize();
-            RunGameLoop();
+            ConsoleGameEngine consoleGameEngine = CreateConsoleGameEngine();
+            consoleGameEngine.RunGameLoop();
         }
 
-        private static void Initialize()
+        private static ConsoleGameEngine CreateConsoleGameEngine()
         {
-            _reader = new ConsoleReader();
-            _writer = new ConsoleWriter();
-            _playerInputRequester = new PlayerInputRequester(_reader, _writer);
-            _gameController = CreateGameController();
-        }
+            var reader = new ConsoleReader();
+            var writer = new ConsoleWriter();
+            var playerInputRequester = new PlayerInputRequester(reader, writer);
+            var consoleGameEngine = new ConsoleGameEngine(reader, writer, playerInputRequester);
 
-        private static void RunGameLoop()
-        {
-            while (_runNewGame)
-            {
-                decimal stake = _playerInputRequester.RequestStake();
-                SlotMachineSpinResultDto result = _gameController.SpinMachine(stake);
-
-                if (!result.IsSuccess)
-                {
-                    _writer.WriteLine(result.ResultMessage);
-                    continue;
-                }
-
-                DrawSymbols(result.Symbols);
-                _writer.WriteLine($"You have won: {result.WinAmount:f2}");
-                _writer.WriteLine($"Current balance is: {result.PlayerBalance:f2}");
-
-                if (!_gameController.PlayerCanPlay())
-                {
-                    _runNewGame = _playerInputRequester.AskForNewGame();
-
-                    if (_runNewGame)
-                    {
-                        _gameController = CreateGameController();
-                    }
-                }
-            }
-        }
-
-        private static GameController CreateGameController()
-        {
-            var gameController = new GameController();
-            decimal deposit = _playerInputRequester.RequestPlayerDeposit();
-            gameController.CreateNewGame(deposit);
-
-            return gameController;
-        }
-
-        private static void DrawSymbols(List<List<Symbol>> rowsOfSymbols)
-        {
-            foreach (var rowOfSymbols in rowsOfSymbols)
-            {
-                _writer.WriteLine(string.Join(", ", rowOfSymbols));
-            }
+            return consoleGameEngine;
         }
     }
 }
